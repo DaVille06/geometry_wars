ba386542b1eb77462e014d2afcd1338350929220
Nick Roberts <roberts.nicholas88@gmail.com>
2023-07-19 12:54:48 -0400

    add files back in projects, and consolidated all components to 1 header file

---
+++ Components.h
@@ -0,0 +1,43 @@
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
+
+#include <SFML/Graphics.hpp>
+#include "Vec2.h"
+
+class CCollision
+{
+public:
+	float radius{0.0f};
+
+	CCollision() {}
+};
+
+class CScore
+{
+public:
+	int score{0};
+
+	CScore() {}
+};
+
+class CShape
+{
+public:
+	sf::CircleShape shape;
+
+	CShape() {}
+};
+
+class CTransform
+{
+public:
+	Vec2 pos;
+	Vec2 speed;
+	Vec2 scale;
+	double angle;
+
+	CTransform();
+	CTransform(const Vec2& p, const Vec2& v);
+};
+
+#endif // !COMPONENTS_H

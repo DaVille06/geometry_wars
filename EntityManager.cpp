780ee7a25ac58ff4c5e721b217a6bbbb99ae2fce
Nick Roberts <roberts.nicholas88@gmail.com>
2023-07-24 15:10:58 -0400

    cleaning up comments of things needing to be done

--- EntityManager.cpp
+++ EntityManager.cpp
@@ -5,23 +5,16 @@ EntityManager::EntityManager() { }
 
 void EntityManager::update()
 {
-	// add entities from m_entitiesToAdd to the proper location(s)
-	// add them to the vector of all entities
-	// add them to the vector inside the map, with the tag as the key
 	for (auto e : m_entitiesToAdd)
 	{
 		m_entities.push_back(e);
 		m_entityMap[e->tag()].push_back(e);
 	}
-	// after we have added entities to add
-	// clear the entity vec
+
 	m_entitiesToAdd.clear();
 
-	// remove dead entities from the vector of all entities
 	removeDeadEntities(m_entities);
 
-	// remove dead entities from each vector in the entity map
-	// c++17 way of iterating through [key, value] pairs in a map
 	for (auto& [tag, entityVec] : m_entityMap)
 	{
 		removeDeadEntities(entityVec);
